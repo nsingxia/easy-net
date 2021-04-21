1165042f03cc1d37fbb914e876488a0eba30ed7d
nsxia <nsxia@sse.com.cn>
2021-04-21 14:21:15 +0800

    add httpex

--- net.go
+++ net.go
@@ -49,6 +49,28 @@ func HttpRaw(method, url string, data io.Reader, headerFun func(request *http.Re
 	return unGzip(body)
 }
 
+func HttpEx(method, url string, data io.Reader, headerFun func(request *http.Request)) ([]byte, error) {
+	client := &http.Client{}
+
+	reqest, err := http.NewRequest(method, url, data)
+
+	if err != nil {
+		return nil, err
+	}
+
+	if headerFun != nil {
+		headerFun(reqest)
+	}
+
+	resp, err := client.Do(reqest)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return ioutil.ReadAll(resp.Body)
+}
+
 func HttpJson(method, url string, data io.Reader, headerFun func(request *http.Request)) (*simplejson.Json, error) {
 	b, err := HttpRaw(method, url, data, headerFun)
 	if err != nil {
